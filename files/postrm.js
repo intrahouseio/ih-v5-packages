0e4fae7502157b1d20efea68174ea712d7322ae3
Artem Ivanov <fast0490f@mail.ru>
2022-12-01 11:18:46 +0300

    update

--- files/postrm.js
+++ files/postrm.js
@@ -4,9 +4,9 @@ function filePostrm(platform, proc, product) {
   return (
     `#!/bin/bash` + '\n' +
     'set -e' + '\n' +
+    'deb-systemd-invoke stop ' + product.service + '.service > /dev/null 2>&1' + '\n' +
+    'deb-systemd-invoke disable ' + product.service + '.service > /dev/null 2>&1' + '\n' +
     'if [ "$1" == "purge" ]; then' + '\n' +
-    '  deb-systemd-invoke stop ' + product.service + '.service > /dev/null 2>&1' + '\n' +
-    '  deb-systemd-invoke disable ' + product.service + '.service > /dev/null 2>&1' + '\n' +
     '  rm -Rf ' + path.join(platform.paths.lib, product.service) + ' > /dev/null 2>&1' + '\n' +
     '  rm -Rf ' + path.join(platform.paths.wd, product.service) + ' > /dev/null 2>&1' + '\n' +
     '  rm -Rf ' + path.join(platform.paths.log, product.service) + ' > /dev/null 2>&1' + '\n' +
