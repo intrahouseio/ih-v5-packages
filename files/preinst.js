c73cef56c72b55c76b0191789553a943b4ca303d
Artem Ivanov <fast0490f@mail.ru>
2023-01-19 11:07:04 +0300

    fix jethome

--- files/preinst.js
+++ files/preinst.js
@@ -6,7 +6,7 @@ function filePreinst(platform, proc, product) {
   return (`#!/bin/bash
 set -e
 
-echo -e "\\033[0;33m Вы устанавливаете стороннее программное обеспечение ${product.name}, по всем возникшим вопросам вы можете обратиться в службу поддержки:\n 1. ${(PRODUCT_SITES[product.name] || '')}\n 2. ${(PRODUCT_TELEGRAM[product.name] || '')}\n 3. support@ih-systems.com \\033[0m"`);
+echo -e "\\033[0;33m Вы устанавливаете стороннее программное обеспечение ${product.name}, по всем возникшим вопросам вы можете обратиться в службу поддержки:\n 1. https://${(PRODUCT_SITES[product.name] || '')}\n 2. ${(PRODUCT_TELEGRAM[product.name] || '')}\n 3. support@ih-systems.com \\033[0m"`);
 }
 
 module.exports = filePreinst;
