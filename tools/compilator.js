fbd3da85b2861de3f564590e2e63dc298c7a3fe0
Artem Ivanov <fast0490f@mail.ru>
2023-09-12 14:31:45 +0300

    rpm

--- tools/compilator.js
+++ tools/compilator.js
@@ -56,7 +56,7 @@ async function compilator(platform, proc, product) {
       cp.on('exit', function(code) {
         const version = global.__versions ? global.__versions[product.name] : VERSION_EMPTY;
         const src = path.join(process.cwd(), rootPath, product.name + '.rpm');
-        const dst = path.join(process.cwd(), (isBeta ? path.join('@builds', 'deb', 'beta') : path.join('@builds', 'deb', 'stable')), `${platform.name}-${product.name}-${version}-1.${proc.arch}.rpm`);
+        const dst = path.join(process.cwd(), (isBeta ? path.join('@builds', 'rpm', 'beta') : path.join('@builds', 'rpm', 'stable')), `${platform.name}-${product.name}-${version}-1.${proc.arch}.rpm`);
 
         fs.moveSync(src, dst, { overwrite: true });
         resolve();
